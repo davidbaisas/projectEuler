458035ef5ff170802d0f708e2c9d83487b4612f3
davidbaisas <156971001+davidbaisas@users.noreply.github.com>
2024-01-17 19:52:21 -0500

    Update problem_16.py

--- problem_16.py
+++ problem_16.py
@@ -2,3 +2,11 @@
 
 # https://projecteuler.net/problem=16
 
+#time module for calculating execution time
+import time
+
+def problem_16():
+  a = list(str(2**1000))
+  a = [int(x) for x in a]
+  pds = sum(a)
+  return pds
